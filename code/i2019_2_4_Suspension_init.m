565ebedb7a1762c78c0c4d4e6064302d250d930e
Joan Vazquez Molina <34097733+joavzmo@users.noreply.github.com>
2019-07-23 16:58:29 +0200

    Add files via upload

---
+++ code/i2019_2_4_Suspension_init.m
@@ -0,0 +1,9 @@
+% suspension.slx initialization file
+
+%% Parameters
+m1 = 2500;          % kg, sprung mass
+m2 = 320;           % kg, unsprung mass
+k1 = 80000;         % N/m, spring constant of suspension
+k2 = 500000;        % N/m, spring constant of tire
+b1 = 350;           % N*s/m damping constant of suspension
+b2 = 15020;         % N*s/m damping constant of tire
